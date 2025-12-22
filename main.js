12708c8f3d08ee8bbe2586bbecc0c0c5b245d262
Pierre <143663492+partglm@users.noreply.github.com>
2025-12-22 21:14:09 +0000

    just do the permuting square and the gen if done

---
+++ main.js
@@ -0,0 +1,4 @@
+import grille from './src/grille.ts'
+
+const a = new grille()
+console.table(grille.mattrice)
--- src/grille.ts
+++ src/grille.ts
@@ -1,127 +1,114 @@
-class grille {
-    static mattrice: number[][] = Array.from({ length: 9 }, () => Array(9).fill(0));
-//    static mattrice: number[][] = [
-//  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-//  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-//  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-//
-//  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-//  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-//  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-//
-//  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-//  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-//  [3, 4, 5, 2, 8, 6, 1, 7, 0]
-//];
+type coordinate = {x: number, y: number}
+type mattrice = (number | null)[][]
+type numbers = {num1: number, num2: number}
+type nullNumber = (null | number)
+type listNumber = (null | number)[]
+
+export default class grille {
+    static mattrice: mattrice = [
+        [8,2,7,1,5,4,3,9,6],
+        [9,6,5,3,2,7,1,4,8],
+        [3,4,1,6,8,9,7,5,2],
+        [5,9,3,4,6,8,2,7,1],
+        [4,7,2,5,1,3,6,8,9],
+        [6,1,8,9,7,2,4,3,5],
+        [7,8,6,2,3,5,9,1,4],
+        [1,5,4,7,9,6,8,2,3],
+        [2,3,9,8,4,1,5,6,7]
+    ];
+    deletedCase: coordinate[] = []
 
     constructor() {
         grille.mattrice = this.generateGrille()
+        this.deletedCase = []
     }
 
-    generateGrille (): number[][] {
-        for (let x=0; x<9; x++) {
-            for (let y=0; y<9; y++) {
-                const result = this.generateNumber(x,y)
-
-                grille.mattrice[x][y] = result
-            }
-        }
+    generateGrille (): mattrice {
+        //this.deletingCase()
+        this.permutingThings()
 
         return grille.mattrice
     }
 
-    generateNumber (x: number, y: number): number {
-        const startTime: number = Date.now();
-        const timeoutMs: number = 1000
-        let number: number = 0;
-
-        do {
-            number = -1
-            const possible: number[] = []
-
-            if ((Date.now() - startTime) > timeoutMs) {
-                console.warn('too long')
-
-                for(let n=1; n<=9; n++){
-                    if(!this.isInColonne(n, y) && !this.isInLigne(n, x) && !this.isInSquare(n, x, y)) {
-                        console.log(n)
-                        possible.push(n)
-                    }
-                }
-
-                const index = Math.floor(Math.random() * possible.length)
-                number = possible[index]
-
-                break
-            }
-
-            number = Math.floor(Math.random() * 9) + 1; 
-        } while (this.isInColonne(number, y) || this.isInLigne(number, x) || this.isInSquare(number, x, y));
-
-        return number;
+    permutingThings (): void {
+        for(let i: number=0; i<this.generateNum(10000, 1000); i++){
+            this.permutingSquare()
+            this.permutingColum()
+            this.permutingLigne()
+        }
     }
 
-    isInSquare (number: number, x: number, y: number): boolean {
-        type Cell = { x: number; y: number };
-        type Grid = { [key in 1|2|3|4|5|6|7|8|9]: Cell[] };
+    generateNumsInSameBlock (): numbers {
+        const block: number = this.generateNum(2)
+        let num1: number = block * 3 + this.generateNum(2)
+        let num2: number = block * 3 + this.generateNum(2)
 
-        const squareById: Grid = {
-            1: [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:0,y:1},{x:1,y:1},{x:2,y:1},{x:0,y:2},{x:1,y:2},{x:2,y:2}],
-            2: [{x:3,y:0},{x:4,y:0},{x:5,y:0},{x:3,y:1},{x:4,y:1},{x:5,y:1},{x:3,y:2},{x:4,y:2},{x:5,y:2}],
-            3: [{x:6,y:0},{x:7,y:0},{x:8,y:0},{x:6,y:1},{x:7,y:1},{x:8,y:1},{x:6,y:2},{x:7,y:2},{x:8,y:2}],
-            4: [{x:0,y:3},{x:1,y:3},{x:2,y:3},{x:0,y:4},{x:1,y:4},{x:2,y:4},{x:0,y:5},{x:1,y:5},{x:2,y:5}],
-            5: [{x:3,y:3},{x:4,y:3},{x:5,y:3},{x:3,y:4},{x:4,y:4},{x:5,y:4},{x:3,y:5},{x:4,y:5},{x:5,y:5}],
-            6: [{x:6,y:3},{x:7,y:3},{x:8,y:3},{x:6,y:4},{x:7,y:4},{x:8,y:4},{x:6,y:5},{x:7,y:5},{x:8,y:5}],
-            7: [{x:0,y:6},{x:1,y:6},{x:2,y:6},{x:0,y:7},{x:1,y:7},{x:2,y:7},{x:0,y:8},{x:1,y:8},{x:2,y:8}],
-            8: [{x:3,y:6},{x:4,y:6},{x:5,y:6},{x:3,y:7},{x:4,y:7},{x:5,y:7},{x:3,y:8},{x:4,y:8},{x:5,y:8}],
-            9: [{x:6,y:6},{x:7,y:6},{x:8,y:6},{x:6,y:7},{x:7,y:7},{x:8,y:7},{x:6,y:8},{x:7,y:8},{x:8,y:8}],
-        };
+        while (num1==num2) {
+            num2 = block * 3 + this.generateNum(2)
+        }
+
+        return { num1, num2 }
+    }
+    
+    permutingSquare (): void {
+         
+    }
+
+    permutingColum (): void {
+        const {num1: colum1, num2: colum2} = this.generateNumsInSameBlock()
+
+        for (let i: number=0; i<9; i++) {
+            const temp: nullNumber = grille.mattrice[i][colum1]
+            grille.mattrice[i][colum1] = grille.mattrice[i][colum2]
+            grille.mattrice[i][colum2] = temp
+        }
+    }
+    
+    permutingLigne (): void {
+        const {num1: ligne1, num2: ligne2} = this.generateNumsInSameBlock()
         
-        const keys: (keyof Grid)[] = [1,2,3,4,5,6,7,8,9];
-        let inWhichId: keyof Grid = 1;
-
-
-        keys.forEach(id => {
-            const cells: Cell[] = squareById[id];
-            cells.forEach(cell => {
-                if (cell.x == x && cell.y == y) {
-                    inWhichId = id
-                }
-            });
-        });
-
-        const numbers: number[] = []
-
-        const square: Cell[] = squareById[inWhichId]
-        square.forEach(pos => {
-            numbers.push(grille.mattrice[pos.x][pos.y])
-        })
-
-        return numbers.includes(number)
+        const temp: listNumber = grille.mattrice[ligne1]
+        grille.mattrice[ligne1] = grille.mattrice[ligne2]
+        grille.mattrice[ligne2] = temp
     }
 
-    isInLigne (number: number, x: number): boolean {
-        for (let y = 0; y < 9; y++) {
-            if (grille.mattrice[x][y] === number) {
-                return true
+    deletingCase (): void {
+        for(let i: number=0; i<50; i++) {
+            const coordinate: coordinate = this.generateCoorOfCase()
+            if (grille.mattrice[coordinate.x][coordinate.y] !== null) {
+                grille.mattrice[coordinate.x][coordinate.y] = null
             }
         }
-        return false
     }
 
-    isInColonne (number: number, y: number): boolean {
-        for (const ligne of grille.mattrice) {
-            if (ligne[y] == number) return true
+    generateNum (intervalle0to: number, minus: (boolean | number)=false): number {
+        let num: number = Math.floor(Math.random() * (intervalle0to + 1))
+        if (minus != false && typeof minus == 'number') {
+            while(num < minus) {
+                num = Math.floor(Math.random() * (intervalle0to + 1))
+            }
         }
-
-        return false
+        return num
     }
 
-    static getNumber (x:number, y:number): number {
+    generateCoorOfCase (): coordinate {
+        let returningValue: coordinate = {x: 0, y:0}
+        do {
+            const colum: number = this.generateNum(8)
+            const ligne: number = this.generateNum(8)
+
+            returningValue = {x: ligne, y: colum}
+        }while(this.deletedCase.some(
+            c => c.x === returningValue.x && c.y === returningValue.y
+        ))
+
+        this.deletedCase = [...this.deletedCase, returningValue]
+        
+        return returningValue
+    }
+
+    static getNumber (x:number, y:number): number | null {
         return grille.mattrice[x][y]
     }
 
 }
-
-const a = new grille()
-console.table(grille.mattrice)
