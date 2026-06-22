79d47f12eecb4bab75cca7d7906909d335bf53d9
partglm <lmpart94012@gmail.com>
2026-06-22 19:16:48 +0200

    help no2

--- a.js
+++ a.js
@@ -1,13 +1,24 @@
-const board=[[null,null,null,null,null,null,1,6,null]
+const board=[[null,null,null,null,null,null,1,6,null],
 [null,null,4,null,null,null,null,null,null],
 [null,null,null,null,1,3,2,null,5],
 [1,4,null,8,6,2,9,null,null],
 [null,7,3,1,null,null,null,2,6],
 [null,null,null,null,null,5,4,null,null],
-[null,8,null,null,2,null,null,5,null],
+[null,8,null,null,2,null,null,null,null],
 [null,9,null,null,4,null,6,null,2],
 [null,null,null,null,3,null,null,4,9]]
 
 import Scan from './dist/evaluation/0-1/scanning.js'
 
-console.table(Scan.scan(board).mattrice)
+const a = Scan.scan(board).mattrice
+console.table(a)
+
+//page 38, parmis garantie optionnel nom + cmb simple indéxé majorés
+
+//2 index equiv, verif tout rentrés + rentrés valerur en dure (bleu)
+// : description + format
+//ecrire dégager 0/ valeur nulle
+ // -> si couleur entete = bleu alors valeur en dure, 
+ //I62
+ //inclure si colonne bleu remettre valeur dans tableau
+  //mettre en carac bleu si en dure
--- src/evaluation/0-1/scanning.ts
+++ src/evaluation/0-1/scanning.ts
@@ -3,51 +3,61 @@ type mattrice = (number | null)[][]
 type coordinate = {x: number, y: number}
 type returnFuncEval = {use: boolean, mattrice: mattrice}
 type coordinateAndNumber = {whereCanBePlace: coordinate, howMuchInBlock: number}
+type coordinateAndBoolean = {whereCanBePlace: coordinate, useful: boolean}
 
 import Possible from '../../solver/possible.js'
 
 export default class Scan {
     static scan(board: mattrice): returnFuncEval {
         let useful: boolean = false
+
         for (let num: number = 1; num <= 9; num++) {        
-            const result: boolean = Scan.board(board, num)
-            if (result) useful = true
+            const result: coordinateAndBoolean = Scan.board(board, num)
+            
+            console.log(result)
+            const pos = result.whereCanBePlace
+            if (result.useful && pos.x != -1) {
+                useful = true
+                console.log(pos)
+                
+                board[pos.x][pos.y] = num
+                break
+            }
         }
         return {use: useful, mattrice: board}
     }
 
-    static board(board: mattrice, num: number): boolean {
+    static board(board: mattrice, num: number): coordinateAndBoolean {
         let useful: boolean = false
-        for (let blocky: number = 1; blocky <= 3; blocky++) {
-            for (let blockx: number = 1; blockx <= 3; blockx++) {
-                let howMuchInBlock: number = 0
-                let whereCanBePlace: coordinate = {x: -1, y: -1}
+        let whereCanBePlace: coordinate = {x: -1, y: -1}
+        for (let block: number = 1; block <= 9; block++) {
+                const arg1HowMuch: number = 0
+                const arg2Where: coordinate = {x: -1, y: -1}
                 
-                Scan.block(board, num, howMuchInBlock, whereCanBePlace)
-
-                if (howMuchInBlock == 1) {
-                    const pos: coordinate = whereCanBePlace
-                    board[pos.x][pos.y] = num
+                let result: coordinateAndNumber = Scan.block(board, num, arg1HowMuch, arg2Where)
+                if (result.howMuchInBlock == 1) {
                     useful = true
+                    whereCanBePlace = result.whereCanBePlace
+                    break
                 }
-
-            }
         }
-        return useful
+        return {useful: useful, whereCanBePlace: whereCanBePlace}
     }
     
     static block(board: mattrice, num: number, howMuchInBlock: number, whereCanBePlace: coordinate): coordinateAndNumber {
-        for (let y: number = 1; y <= 3; y++) {
-            for (let x: number = 1; x <= 3; x++) {
+        let where: coordinate = whereCanBePlace
+        let HowMuch: number = howMuchInBlock
+        for (let x: number = 1; x < 3; x++) {
+            for (let y: number = 1; y < 3; y++) {
                 const possible: Possible = new Possible(board)
                 const isPossible: boolean = possible.isPossible({x: x, y: y}, num)
                 
                 if (isPossible) {
-                    howMuchInBlock++
-                    whereCanBePlace = {x: x, y: y}
+                    HowMuch++
+                    where = {x: x, y: y}
                 }
             }
         }
-        return {whereCanBePlace, howMuchInBlock}
+        return {whereCanBePlace: where, howMuchInBlock: HowMuch}
     }
 }
